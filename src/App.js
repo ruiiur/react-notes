b9ed7f7adad0268213eda5b050ebdaafb20a9f37
duguili <dugl@yidianling.com>
2021-07-28 10:45:25 +0800

    tip

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+// ---JSX，即JavaScript XML，一种在React组建内部构建标签的类XML语法。(增强React程序组件的可读性)
+
+// ---绑定属性注意
+//    class变成className
+//    for变成htmlFor
+//       ---这是因为class和for都是js里面的关键字
+//    style属性跟以前的写法有些不一样
+//      <span style={{marginLeft:'2px'}}>hello</span>
+
+// ---组件名和类名首字母大写
+
 import './App.css';
 import TestHook from './components/TestHook';
 import TestClassExample from './components/TestClassExample';
